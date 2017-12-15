0de9665d1ef7ef7f4d82eef8cb94b25e8e8c73ca
William Brooks <brooks.william@gmail.com>
2017-12-15 15:40:25 -0800

    completed yarn build

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,21 +2,6 @@ import axios from "axios";
 
 export default {
 
-    //gets new articles from api/search route
-    searchArticles: function(topic, begin, end) {
-
-        console.log('searchArticles (API.js)', 'topic:', topic, 'begin:', begin, 'end:', end);     
-        //console.log("axios.get api/search", axios.get("/api/search", { params: { q: topic, begin_date: begin, end_date: end }}));
-
-        return axios.get("/api/search", { query: { q: topic, begin_date: begin, end_date: end } });
-
-        // queryURLBase is the start of our API endpoint.
-        // const queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=";
-        // const authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
-        // const queryURL = queryURLBase + authKey + "q=" + topic;
-        // console.log(`queryURL: ${queryURL}`);
-    },
-
     // Gets all articles
     getSavedArticles: function() {
         return axios.get("/api/articles");
